de32a60e48ee542b5e0453de793b44f3e517f25a
Rahul Raj <me.rahulraj07@gmail.com>
2022-07-13 18:13:15 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 import postRoutes from "./routes/posts.js";
 
@@ -13,12 +15,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://rahulraj:noobcoder1994@cluster0.p6sfh.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
